02f1d59ccca077a8d9d4d63d94ecccf1375918b8
unknown <itshakf@outlook.fr>
2020-06-02 13:36:05 +0300

    wait for cr 1 after merge from master

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,7 @@
 import { ActionReducerMap } from '@ngrx/store';
-
+import { cartKey, cartReducer as cartReducer } from './cart/reducer/cart-reducer';
 import { productKey, reducer as productReducer } from './main-dashboard/reducers/dashboard-reducer';
-import { cartKey, reducer as cartReducer } from './cart/reducer/cart-reducer';
+
 
 export const reducers: ActionReducerMap<any> = {
   [productKey]: productReducer,
